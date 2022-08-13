d301e8c1308540c19fbde8d4fd0c172daab9899c
dev-mkr <webdev.mu@gamil.com>
2022-08-14 01:21:28 +0200

    Saving board data to local storge 🚀

--- src/features/kanban-board/Kanban.js
+++ src/features/kanban-board/Kanban.js
@@ -19,24 +19,28 @@ const initialData = {
     },
     "task-3": {
       id: "task-3",
-      content: "publish video",
+      content: "publish video 🚀",
+    },
+    "task-4": {
+      id: "task-4",
+      content: "finish English course 🚀",
     },
   },
   columns: {
     "column-1": {
       id: "column-1",
       title: "To do",
-      taskIds: ["task-1", "task-2", "task-3"],
+      taskIds: ["task-1", "task-2"],
     },
     "column-2": {
       id: "column-2",
       title: "In progress",
-      taskIds: [],
+      taskIds: ["task-3"],
     },
     "column-3": {
       id: "column-3",
       title: "Done",
-      taskIds: [],
+      taskIds: ["task-4"],
     },
   },
   columnOrder: ["column-1", "column-2", "column-3"],

b9cf9926247decfe80fdd628ca861aca8442dca4
dev-mkr <webdev.mu@gamil.com>
2022-08-15 20:50:41 +0200

    enhance developer experience 🧼

--- src/features/kanban-board/Kanban.js
+++ src/features/kanban-board/Kanban.js
@@ -7,51 +7,39 @@ import { AddNewColumn } from "./components/AddNewColumn";
 import reducerFunction from "./utilities/reducerFunction";
 import handelDragEnd from "./utilities/handelDragEnd";
 import useLocalStorage from "../../Hooks/useLocalStorage";
-const initialData = {
-  tasks: {
-    "task-1": {
-      id: "task-1",
-      content: "create video",
-    },
-    "task-2": {
-      id: "task-2",
-      content: "edit video",
-    },
-    "task-3": {
-      id: "task-3",
-      content: "publish video 🚀",
-    },
-    "task-4": {
-      id: "task-4",
-      content: "finish English course 🚀",
-    },
-  },
-  columns: {
-    "column-1": {
-      id: "column-1",
-      title: "To do",
-      taskIds: ["task-1", "task-2"],
-    },
-    "column-2": {
-      id: "column-2",
-      title: "In progress",
-      taskIds: ["task-3"],
-    },
-    "column-3": {
-      id: "column-3",
-      title: "Done",
-      taskIds: ["task-4"],
-    },
-  },
-  columnOrder: ["column-1", "column-2", "column-3"],
-};
+import initialData from "../../data/initialData";
 
 function Kanban() {
-  const [kanbanData, setKanbanData] = useLocalStorage("kanbanData", initialData);
+  const [kanbanData, setLocalKanbanData] = useLocalStorage("kanbanData", initialData);
   const [globalState, dispatch] = useReducer(reducerFunction, kanbanData);
   useEffect(() => {
-    setKanbanData(globalState);
+    setLocalKanbanData(globalState);
   }, [globalState]);
+
+  const columns = globalState.columnOrder.map((columnId, index) => {
+    const column = globalState.columns[columnId];
+    const tasks = column.taskIds.map((taskId) => globalState.tasks[taskId]);
+    return (
+      <Column
+        key={column.id}
+        columnTitle={column.title}
+        columnId={column.id}
+        dispatch={dispatch}
+        index={index}
+      >
+        {tasks.map((task, index) => (
+          <Task
+            key={task.id}
+            taskId={task.id}
+            content={task.content}
+            index={index}
+            columnId={column.id}
+            dispatch={dispatch}
+          />
+        ))}
+      </Column>
+    );
+  });
 
   return (
     <DragDropContext onDragEnd={(result) => handelDragEnd(result, globalState, dispatch)}>
@@ -62,32 +50,7 @@ function Kanban() {
             {...provided.droppableProps}
             ref={provided.innerRef}
           >
-            {globalState.columnOrder.map((columnId, index) => {
-              const column = globalState.columns[columnId];
-
-              const tasks = column.taskIds.map((taskId) => globalState.tasks[taskId]);
-              return (
-                <Column
-                  key={column.id}
-                  columnTitle={column.title}
-                  columnId={column.id}
-                  dispatch={dispatch}
-                  index={index}
-                >
-                  {tasks.map((task, index) => (
-                    <Task
-                      key={task.id}
-                      taskId={task.id}
-                      content={task.content}
-                      index={index}
-                      columnId={column.id}
-                      dispatch={dispatch}
-                    />
-                  ))}
-                </Column>
-              );
-            })}
-
+            {columns}
             {provided.placeholder}
             <AddNewColumn dispatch={dispatch} />
           </section>

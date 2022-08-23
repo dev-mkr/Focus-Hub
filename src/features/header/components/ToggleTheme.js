188ef58c18b71d33ce607d653641b8e7113514a3
dev-mkr <webdev.mu@gamil.com>
2022-08-23 20:46:42 +0200

    Adding sounds when toggle theme ✨

--- src/features/header/components/ToggleTheme.js
+++ src/features/header/components/ToggleTheme.js
@@ -3,10 +3,15 @@ import Button from "components/Button";
 import useLocalStorage from "Hooks/useLocalStorage";
 import { ReactComponent as DarkThemeIcon } from "assets/darkThemeIcon.svg";
 import { ReactComponent as LightThemeIcon } from "assets/lightThemeIcon.svg";
+import switchon from "assets/switchon.mp3";
+import switchoff from "assets/switchoff.mp3";
 const ToggleTheme = () => {
   const [theme, setTheme] = useLocalStorage("theme");
 
   const handelClick = () => {
+    const sound = new Audio();
+    sound.src = theme === "light" ? switchoff : switchon;
+    sound.play();
     document.documentElement.classList.toggle(`dark-theme`);
     setTheme(theme === "light" ? "dark" : "light");
   };
